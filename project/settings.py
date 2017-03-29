9cc70dd096c3ac41e527e8a276e3fc7b1bb811af
danielrubiovi <danielru_1994@hotmail.com>
2017-03-29 17:03:33 +0200

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import calc
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calc',
 )
 
 MIDDLEWARE_CLASSES = (
