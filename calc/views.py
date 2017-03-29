5ee8c8a3ad030a4a9bbcb22da500e5dc2b80166d
danielrubiovi <danielru_1994@hotmail.com>
2017-03-29 17:06:27 +0200

    views.py

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def operaciones (self,num1,operando,num2):
+
+	if operando == '+':
+		suma = int(num1) + int(num2)
+		htmlAnswer = '<body><h3> La suma da: '
+		htmlAnswer += str(suma) + '</h3></body>'
+
+	elif operando == '-':
+		resta = int(num1) - int(num2)
+		htmlAnswer = '<body><h3> La resta da: '
+		htmlAnswer += str(resta) + '</h3></body>'
+
+	elif operando == '/':
+		division = int(num1) / int(num2)
+		htmlAnswer = '<body><h3> La division da: '
+		htmlAnswer += str(division) + '</h3></body>'
+
+	elif operando == '*':
+		multiplicacion = int(num1) * int(num2)
+		htmlAnswer = '<body><h3> La multiplicacion da: '
+		htmlAnswer += str(multiplicacion) + '</h3></body>'
+
+	else:
+		htmlAnswer = '404 Not Found'
+
+	return HttpResponse (htmlAnswer)
