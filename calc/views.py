5f31d6d5eb0f96888366685dde3037c55a70fc6f
danielrubiovi <danielru_1994@hotmail.com>
2017-03-29 17:16:18 +0200

    cambio views.py

--- calc/views.py
+++ calc/views.py
@@ -5,24 +5,30 @@ from django.http import HttpResponse
 
 def operaciones (self,num1,operando,num2):
 
+	print ('NUM 2:'+ str(num2))
+
 	if operando == '+':
 		suma = int(num1) + int(num2)
-		htmlAnswer = '<body><h3> La suma da: '
+		htmlAnswer = '<body><h3> La suma de '
+		htmlAnswer += str(num1) + str(operando) + str(num2) + ' = '
 		htmlAnswer += str(suma) + '</h3></body>'
 
 	elif operando == '-':
 		resta = int(num1) - int(num2)
-		htmlAnswer = '<body><h3> La resta da: '
+		htmlAnswer = '<body><h3> La resta de '
+		htmlAnswer += str(num1) + str(operando) + str(num2) + ' = '
 		htmlAnswer += str(resta) + '</h3></body>'
 
 	elif operando == '/':
 		division = int(num1) / int(num2)
-		htmlAnswer = '<body><h3> La division da: '
+		htmlAnswer = '<body><h3> La division de '
+		htmlAnswer += str(num1) + str(operando) + str(num2) + ' = '
 		htmlAnswer += str(division) + '</h3></body>'
 
 	elif operando == '*':
 		multiplicacion = int(num1) * int(num2)
-		htmlAnswer = '<body><h3> La multiplicacion da: '
+		htmlAnswer = '<body><h3> La multiplicacion de '
+		htmlAnswer += str(num1) + str(operando) + str(num2) + ' = '
 		htmlAnswer += str(multiplicacion) + '</h3></body>'
 
 	else:
